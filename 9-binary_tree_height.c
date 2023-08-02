6032ef932a46e7e0f7bace4f4927b7e277321bf1
Robert Amoah <mr_robertamoah@yahoo.com>
2023-08-02 21:15:43 +0100

    done with 9-binary_tree_height.c

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_height - measures the height of a binary tree
+* @tree: pointer to binary_tree_t
+*
+* Return: size_t
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t right_h = 0, left_h = 0, to_add = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+
+	if (tree->left || tree->right)
+		to_add = 1;
+
+	if (right_h >= left_h)
+		return (to_add + right_h);
+	else
+		return (to_add + left_h);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -35,5 +36,6 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
